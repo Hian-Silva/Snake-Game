cde67e790d37db4cb24a1deddcbf542d1c2ced24
Hian-Silva <98980616+Hian-Silva@users.noreply.github.com>
2022-02-08 11:27:22 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const exphbs = require('express-handlebars')
+
+app.use('/public', express.static('public'))
+
+app.engine('.hbs', exphbs.engine({
+  defaultLayout: 'main', extname: 'hbs'
+}))
+
+app.set('view engine', '.hbs')
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.listen(3000, () => {
+  console.log('Backend executando...')
+})
